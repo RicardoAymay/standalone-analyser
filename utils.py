594f4212678db80d98a829eed44961404e0fb0d1
RicardoAymay <r.aymay.goncalves@gmail.com>
2024-10-27 11:20:54 -0300

    uncommit

---
+++ utils.py
@@ -0,0 +1,13 @@
+DAYS_CANDLE_COUNTER = {
+    "M1": 1440,
+    "M5" : 288,
+    "M15" : 96,
+    "M30" : 48,
+    "H1" : 24,
+    "H4" : 6,
+    "D1" : 1,
+    "W1" : 1,
+}
+
+def getAssociatedCandles():
+    pass
